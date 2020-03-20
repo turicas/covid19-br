ab8b7a0fd5663d73b6c552b6825fd993b8056779
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-03-20 15:56:55 -0300

    Adiciona CSVs com o resultado da coleta

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ set -e
 mkdir -p data/download data/output data/log
 
 for spider in caso boletim; do
+	rm -rf data/output/${spider}.csv*
 	time scrapy runspider consolida.py \
 		--loglevel=INFO \
 		--logfile=data/log/${spider}.log \
 		-a input_filename=data/${spider}_url.csv \
 		-o data/output/${spider}.csv
+	git add -f data/output/${spider}.csv
 	gzip data/output/${spider}.csv
 done
