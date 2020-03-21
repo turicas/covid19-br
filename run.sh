233026cda2a0d13aa54149e316a4922303bc0460
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-03-21 10:27:27 -0300

    Ordena CSV de boletins e casos

--- run.sh
+++ run.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+function sort_csv() {
+	filename=$1
+
+	tempfile=$(mktemp)
+	head -1 "$filename" > $tempfile
+	tail +2 "$filename" | sort >> "$tempfile"
+	mv "$tempfile" "$filename"
+}
+
 set -e
 mkdir -p data/download data/output data/log
 
 for table in caso boletim; do
-	rm -rf data/output/${table}.csv*
+	output_filename="data/output/${table}.csv"
+	rm -rf "$output_filename" "${output_filename}.gz"
 	time scrapy runspider consolida.py \
 		--loglevel=INFO \
 		--logfile=data/log/${table}.log \
 		-a input_filename=data/${table}_url.csv \
-		-o data/output/${table}.csv
+		-o "$output_filename"
+	sort_csv $output_filename
 done
