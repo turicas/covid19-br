ae097dca52546f987a227cde7d974451eb9b15b4
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-03-26 00:30:53 -0300

    Corrige consolidação de dados

--- consolida.py
+++ consolida.py
@@ -15,7 +15,9 @@ def gdocs_xlsx_download_url(url):
 
 class ConsolidaSpider(scrapy.Spider):
     name = "consolida"
-    start_urls = ["https://docs.google.com/spreadsheets/u/0/d/1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI/export?format=csv&id=1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI&gid=0"]
+    start_urls = [
+        "https://docs.google.com/spreadsheets/u/0/d/1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI/export?format=csv&id=1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI&gid=0"
+    ]
 
     def __init__(self, boletim_filename, caso_filename, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,28 +29,21 @@ class ConsolidaSpider(scrapy.Spider):
             force_types={
                 "populacao_estimada": rows.fields.IntegerField,
                 "codigo_municipio": rows.fields.TextField,
-            }
+            },
         )
         self.population_per_city = {
-            (city.uf, city.municipio): city.populacao_estimada
-            for city in population
+            (city.uf, city.municipio): city.populacao_estimada for city in population
         }
         self.city_code = {
-            (city.uf, city.municipio): city.codigo_municipio
-            for city in population
+            (city.uf, city.municipio): city.codigo_municipio for city in population
         }
-        self.state_code = {
-            city.uf: city.codigo_uf
-            for city in population
-        }
+        self.state_code = {city.uf: city.codigo_uf for city in population}
         self.population_per_state = Counter()
         for city in population:
             self.population_per_state[city.uf] += city.populacao_estimada
 
     def parse(self, response):
-        table = rows.import_from_csv(
-            io.BytesIO(response.body), encoding="utf-8"
-        )
+        table = rows.import_from_csv(io.BytesIO(response.body), encoding="utf-8")
         for row in table:
             yield scrapy.Request(
                 gdocs_xlsx_download_url(row.planilha_brasilio),
@@ -84,26 +79,29 @@ class ConsolidaSpider(scrapy.Spider):
             self.boletim_writer.writerow(boletim)
 
         self.logger.info(f"Parsing {state} caso")
-        casos = rows.import_from_xlsx(io.BytesIO(response.body), sheet_name="Casos (FINAL)")
+        casos = rows.import_from_xlsx(
+            io.BytesIO(response.body), sheet_name="Casos (FINAL)"
+        )
         cities = defaultdict(dict)
         for caso in casos:
             caso = caso._asdict()
-            caso_data = [item for item in caso.values() if item]
+            caso_data = [
+                value
+                for key, value in caso.items()
+                if key != "municipio" and value is not None
+            ]
             if not caso_data:
                 continue
-            elif not caso["municipio"].strip():
-                if not caso[key]:
-                    continue
-                else:
-                    self.logger.warning(f"ERROR PARSING (empty city) {caso}")
             for key, value in caso.items():
                 if key == "municipio":
                     continue
-                if key.startswith("confirmados_") or key.startswith("mortes_"):
+                elif key.startswith("confirmados_") or key.startswith("mortes_"):
                     try:
                         _, day, month = key.split("_")
                     except ValueError:
-                        self.logger.error(f"ERROR PARSING {repr(key)} - {repr(value)} - {caso}")
+                        self.logger.error(
+                            f"ERROR PARSING {repr(key)} - {repr(value)} - {caso}"
+                        )
                         continue
                     date = f"2020-{int(month):02d}-{int(day):02d}"
                     if key.startswith("confirmados_"):
@@ -115,9 +113,11 @@ class ConsolidaSpider(scrapy.Spider):
                 if date not in cities[caso["municipio"]]:
                     cities[caso["municipio"]][date] = {}
                 try:
-                    value = int(value) if value else ""
+                    value = int(value) if value not in (None, "") else None
                 except ValueError:
-                    self.logger.error(f"ERROR converting to int: {date} {number_type} {value} {caso}")
+                    self.logger.error(
+                        f"ERROR converting to int: {date} {number_type} {value} {caso}"
+                    )
                     continue
                 cities[caso["municipio"]][date][number_type] = value
         result = []
@@ -125,10 +125,10 @@ class ConsolidaSpider(scrapy.Spider):
             for date, date_data in city_data.items():
                 confirmed = date_data["confirmed"]
                 deaths = date_data["deaths"]
-                if not confirmed and not deaths:
+                if confirmed is None and deaths is None:
                     continue
-                confirmed = int(confirmed) if confirmed else 0
-                deaths = int(deaths) if deaths else 0
+                confirmed = int(confirmed) if confirmed is not None else None
+                deaths = int(deaths) if deaths is not None else None
                 row = {
                     "date": date,
                     "state": state,
@@ -152,20 +152,30 @@ class ConsolidaSpider(scrapy.Spider):
                 else:
                     state_code = int(self.state_code[row["state"]])
                     city_code = int(self.city_code[(row["state"], row["city"])])
-                    row_population = self.population_per_city[(row["state"], row["city"])]
-                    row_city_code = f'{state_code:02d}{city_code:05d}'
+                    row_population = self.population_per_city[
+                        (row["state"], row["city"])
+                    ]
+                    row_city_code = f"{state_code:02d}{city_code:05d}"
             elif row["place_type"] == "state":
                 state_code = int(self.state_code[row["state"]])
                 row_population = self.population_per_state[row["state"]]
-                row_city_code = f'{state_code:02d}'
+                row_city_code = f"{state_code:02d}"
             else:
                 self.logger.error(f"Invalid row: {row}")
                 continue
-            confirmed_per_100k = 100_000 * (row["confirmed"] / row_population) if row_population else None
-            death_rate = row["deaths"] / row["confirmed"] if row["confirmed"] else None
+            row_deaths = row["deaths"]
+            row_confirmed = row["confirmed"]
+            confirmed_per_100k = (
+                100_000 * (row_confirmed / row_population)
+                if row_confirmed and row_population
+                else None
+            )
+            death_rate = row_deaths / row_confirmed if row_deaths and row_confirmed else None
             row["estimated_population_2019"] = row_population
             row["city_ibge_code"] = row_city_code
-            row["confirmed_per_100k_inhabitants"] = f"{confirmed_per_100k:.5f}" if confirmed_per_100k else None
+            row["confirmed_per_100k_inhabitants"] = (
+                f"{confirmed_per_100k:.5f}" if confirmed_per_100k else None
+            )
             row["death_rate"] = f"{death_rate:.4f}" if death_rate else None
             self.logger.debug(row)
             self.caso_writer.writerow(row)
