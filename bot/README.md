44400a99b8ccc569a1ad6d6870e1ff081d022933
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-03-27 12:48:21 -0300

    Adiciona envio de mensagem em Python e Bash

---
+++ bot/README.md
@@ -0,0 +1,32 @@
+# Bot RocketChat (chat.brasil.io)
+
+## Python
+
+`rocketchat.py`: pode ser usado como biblioteca Python ou script que envia
+mensagem pela linha de comando. Funções implementadas: login, criação de
+usuário de bot e envio de mensagem.
+
+Script:
+
+```shell
+python rocketchat.py --base_url=<base_url> --username=<username> --password=<password> "#channel" "message"
+python rocketchat.py --base_url=<base_url> --user_id=<user_id> --auth_token=<auth_token> "#channel" "message"
+```
+
+Biblioteca: veja o `if __name__ == "__main__"` do `rocketchat.py`.
+
+
+## Bash
+
+`rocket_functions.sh`: funções Bash para login, criação de usuário de bot e
+envio de mensagem
+
+Funções disponíveis:
+
+```shell
+source rocket_functions.sh
+
+rocket_user_login <base_url> <username> <password>
+rocket_user_create_bot_user <base_url> <user_id> <auth_token> <username> <password> <email> <name>
+rocket_msg_send <base_url> <user_id> <auth_token> <channel> <message>
+```
---
+++ bot/rocketchat.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+from urllib.parse import urljoin
+import requests
+
+
+HTTP_METHODS = "GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH".lower().split()
+
+
+class RocketChat:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def make_url(self, endpoint):
+        return urljoin(self.base_url, f"/api/v1/{endpoint}")
+
+    def make_request(self, method, *args, **kwargs):
+        method = method.lower().strip()
+        assert method in HTTP_METHODS
+
+        kwargs["headers"] = kwargs.get("headers", {})
+        kwargs["headers"]["X-Auth-Token"] = self.auth_token
+        kwargs["headers"]["X-User-Id"] = self.user_id
+        return getattr(requests, method)(*args, **kwargs)
+
+    def login(self, username, password):
+        self.username = username
+        response = requests.post(
+            self.make_url("login"), data={"user": username, "password": password}
+        )
+        data = response.json()
+        assert data["status"] == "success"
+        self.user_id = data["data"]["userId"]
+        self.auth_token = data["data"]["authToken"]
+        self.user_data = data["data"]["me"]
+
+    def create_bot_user(self, bot_username, bot_password, bot_email, bot_name):
+        return self.make_request(
+            "POST",
+            self.make_url("users.create"),
+            json={
+                "username": bot_username,
+                "password": bot_password,
+                "email": bot_email,
+                "name": bot_name,
+                "active": True,
+                "roles": ["bot"],
+                "joinDefaultChannels": False,
+                "requirePasswordChange": False,
+                "sendWelcomeEmail": False,
+                "verified": True,
+            },
+        )
+
+    def send_message(self, channel, message):
+        return self.make_request(
+            "POST",
+            self.make_url("chat.postMessage"),
+            json={"channel": channel, "text": message,},
+        )
+
+
+if __name__ == "__main__":
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_url")
+    parser.add_argument("--username")
+    parser.add_argument("--password")
+    parser.add_argument("--user_id")
+    parser.add_argument("--auth_token")
+    parser.add_argument("channel")
+    parser.add_argument("message")
+    args = parser.parse_args()
+
+    base_url = args.base_url or os.environ["ROCKETCHAT_BASE_URL"]
+
+    username = args.username or os.environ.get("ROCKETCHAT_USERNAME")
+    password = args.password or os.environ.get("ROCKETCHAT_PASSWORD")
+    user_id = args.user_id or os.environ.get("ROCKETCHAT_USER_ID")
+    auth_token = args.auth_token or os.environ.get("ROCKETCHAT_AUTH_TOKEN")
+    if (
+        (username, password, user_id, auth_token) == (None, None, None, None)
+        or
+        (username and not password)
+        or
+        (user_id and not auth_token)
+    ):
+        print("ERROR: must pass username and password or user_id and auth_token")
+        exit(1)
+
+    chat = RocketChat(base_url)
+    if username and password:
+        chat.login(username, password)
+    elif user_id and auth_token:
+        chat.user_id = user_id
+        chat.auth_token = auth_token
+    else:
+        print("ERROR: cannot authenticate")
+        exit(2)
+    chat.send_message(args.channel, args.message)
