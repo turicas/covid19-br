e38770233096e59f6257d2f01ec6eb10bf56f876
Bjverde <bjverde@yahoo.com.br>
2020-04-13 22:57:17 -0300

    Informando estado que está coletando os dados

--- collect.sh
+++ collect.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Cores
+RED='\033[0;31m'
+LIGHT_GREEN='\033[0;32m'
+YBLUE='\033[1;33;1;44m'
+NC='\033[0m' # No Color
+
 set -e
 if [[ $OSTYPE == "darwin"* ]]; then
   SCRIPT_PATH=$(dirname $(pwd)/$(basename $0))
@@ -7,10 +13,13 @@ else
   SCRIPT_PATH=$(dirname $(readlink -f $0))
 fi
 
+echo -e "${LIGHT_GREEN}\n Coletando dados Brasil \n${NC}"
+
 source $SCRIPT_PATH/base.sh
 
 mkdir -p $DOWNLOAD_PATH $OUTPUT_PATH $LOG_PATH
 for state in ce pr rr sp; do
+	echo -e "${YBLUE} Estado: ${state} ${NC}"
 	log_filename="$LOG_PATH/caso-${state}.log"
 	csv_filename="$OUTPUT_PATH/caso-${state}.csv"
 	rm -rf "$log_filename" "$csv_filename"
@@ -18,5 +27,5 @@ for state in ce pr rr sp; do
 		--loglevel=INFO \
 		--logfile="$log_filename" \
 		-o "$csv_filename" \
-		corona_${state}_spider.py
+		corona_${state}_spider.py	
 done
