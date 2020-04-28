d2190b8905ab19adb50c0053c2e3379376e30168
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-28 09:26:15 -0300

    Corrige spider PE

--- corona_pe_spider.py
+++ corona_pe_spider.py
@@ -11,12 +11,51 @@ import scrapy
 
 BASE_PATH = Path(__file__).parent
 POPULATION_PATH = BASE_PATH / "data" / "populacao-estimada-2019.csv"
-PE_CITIES = {
+population_pe = [
+    row
+    for row in rows.import_from_csv(POPULATION_PATH)
+    if row.state == "PE"
+]
+CITY_NAME_BY_ID = {
     int(str(row.city_ibge_code)[:-1]): row.city
+    for row in population_pe
+}
+CITY_NAME_BY_ID[0] = "Importados/Indefinidos"
+CITY_NAME_BY_SLUG = {
+    rows.fields.slug(row.city): row.city
+    for row in population_pe
+}
+CITY_NAME_BY_SLUG[rows.fields.slug("Importados/Indefinidos")] = "Importados/Indefinidos"
+CITY_ID_BY_NAME = {
+    row.city: int(str(row.city_ibge_code)[:-1])
     for row in rows.import_from_csv(POPULATION_PATH)
     if row.state == "PE"
 }
-PE_CITIES[0] = "Importados/Indefinidos"
+CITY_ID_BY_NAME["Importados/Indefinidos"] = 0
+
+
+def fix_row(row):
+    new = row.copy()
+    if int(new["cd_municipio"]) == 0 or not new["cd_municipio"]:
+        municipio = new["mun_notificacao"]
+        if municipio in ("OUTRO ESTADO", "OUTRO PAÍS"):
+            new["cd_municipio"] = 0
+        else:
+            if municipio.endswith("GUA PRETA"):
+                municipio = "Água Preta"
+            elif not municipio:
+                municipio = "Importados/Indefinidos"
+            else:
+                municipio = municipio.encode("iso-8859-1").decode("utf-8").title().replace(" Do ", " do ").replace(" Da ", " da ").replace(" De ", " de ")
+            try:
+                municipio = CITY_NAME_BY_SLUG[rows.fields.slug(municipio)]
+            except KeyError:
+                print(f"Error converting city in PE: {municipio}")
+                municipio = "Importados/Indefinidos"
+            new["mun_notificacao"] = municipio
+            new["cd_municipio"] = CITY_ID_BY_NAME[municipio]
+
+    return new
 
 
 class Covid19PESpider(scrapy.Spider):
@@ -41,7 +80,7 @@ class Covid19PESpider(scrapy.Spider):
         result = []
         for row in zip(*case_data):
             row = dict(zip(header, row))
-            result.append(row)
+            result.append(fix_row(row))
 
         now = datetime.datetime.now()
         date = datetime.date(now.year, now.month, now.day)
@@ -53,7 +92,7 @@ class Covid19PESpider(scrapy.Spider):
             confirmed = len(city_data)
             deaths = sum(1 for row in city_data if row["evolucao"] == "ÓBITO")
             row = {
-                "municipio": PE_CITIES[city_ibge_code],
+                "municipio": CITY_NAME_BY_ID[city_ibge_code],
                 "confirmados": confirmed,
                 "mortes": deaths,
             }
