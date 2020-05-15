63fd3e174bf39371a68c0954f6c71271854bc49b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-05-15 18:35:26 -0300

    Usa BaseRegistroCivilSpider em TotalDeathsSpider

--- obitos_totais_spider.py
+++ obitos_totais_spider.py
@@ -2,44 +2,40 @@ import datetime
 import json
 from urllib.parse import urlencode, urljoin
 
-import scrapy
-
 import date_utils
-from obitos_spider import STATES, qs_to_dict
+from obitos_spider import BaseRegistroCivilSpider, STATES, qs_to_dict
 
 
-class DeathsSpider(scrapy.Spider):
+class TotalDeathsSpider(BaseRegistroCivilSpider):
     name = "obitos_totais"
     base_url = "https://transparencia.registrocivil.org.br/api/record/death"
+    start_date = datetime.date(2015, 1, 1)
+    end_date = date_utils.today()
 
-
-    def make_request(
-        self, start_date, end_date, state, callback, dont_cache=False
-    ):
+    def make_state_request(self, start_date, end_date, state, callback, dont_cache=False):
         data = [
             ("start_date", str(start_date)),
             ("end_date", str(end_date)),
             ("state", state),
         ]
-        return scrapy.Request(
+        return self.make_request(
             url=urljoin(self.base_url, "?" + urlencode(data)),
             callback=callback,
             meta={"row": qs_to_dict(data), "dont_cache": dont_cache},
         )
 
-    def start_requests(self):
-        # TODO: criar BaseRegistroCivilSpider com start_requests e herdar dela
+    def start_requests_after_login(self):
         one_day = datetime.timedelta(days=1)
         today = date_utils.today()
-        tomorrow = today + datetime.timedelta(days=1)
-        # `date_utils.date_range` excludes the last, so to get today's data we
-        # need to pass tomorrow.
-        for date in date_utils.date_range(datetime.date(2015, 1, 1), tomorrow, interval="monthly"):
+        non_cache_period = datetime.timedelta(days=30)
+        # `date_utils.date_range` excludes the last, so we need to add one day
+        # to `end_date`.
+        for date in date_utils.date_range(self.start_date, self.end_date + one_day, interval="monthly"):
             # Won't cache dates from 30 days ago until today (only historical
             # ones which are unlikely to change).
-            should_cache = today - date > datetime.timedelta(days=30)
+            should_cache = today - date > non_cache_period
             for state in STATES:
-                yield self.make_request(
+                yield self.make_state_request(
                     start_date=date,
                     end_date=date_utils.next_date(date, "monthly") - one_day,
                     state=state,
