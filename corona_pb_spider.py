fa8d3649198aa100bf1ac8b2f8a8ab0e7474c43e
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2020-06-07 11:03:19 -0300

    extrai código duplicado  no spider da PB

--- corona_pb_spider.py
+++ corona_pb_spider.py
@@ -20,10 +20,12 @@ class PBSpider(scrapy.Spider):
             if url.startswith('https://paraiba.pb.gov.br/diretas/saude/coronavirus/noticias/atualizacao-covid-19'):
                 yield response.follow(url, self.parse)
 
+        paragraphs = response.css('p')
+
         yield {
-            "date": self.__get_date(response),
-            "deaths": self.__get_total_deaths(response),
-            "confirmed": self.__get_total_cases(response)
+            "date": self.__get_date(paragraphs),
+            "deaths": self.__get_total_deaths(paragraphs),
+            "confirmed": self.__get_total_cases(paragraphs)
         }
 
     def __get_date(self, response):
@@ -32,23 +34,23 @@ class PBSpider(scrapy.Spider):
 
         return f'{day_and_month}2020'
 
-    def __get_total_cases(self, response):
-        cell_text, *_ = self.__get_confirmed_paragraph(response)
+    def __get_total_cases(self, paragraphs):
+        cell_text, *_ = self.__get_confirmed_paragraph(paragraphs)
 
         return self.__extract_number(cell_text)
 
-    def __get_confirmed_paragraph(self, response):
-        return [p.get() for p in response.css('p') if 'Casos Confirmados' in p.get()] or \
-               [p.get() for p in response.css('p') if 'TOTAL CONFIRMADOS' in p.get()]
+    def __get_confirmed_paragraph(self, paragraphs):
+        return [p.get() for p in paragraphs if 'Casos Confirmados' in p.get()] or \
+               [p.get() for p in paragraphs if 'TOTAL CONFIRMADOS' in p.get()]
 
-    def __get_total_deaths(self, response):
-        cell_text, *_ = self.__get_deaths_paragraph(response)
+    def __get_total_deaths(self, paragraphs):
+        cell_text, *_ = self.__get_deaths_paragraph(paragraphs)
 
         return self.__extract_number(cell_text)
 
-    def __get_deaths_paragraph(self, response):
-        return [p.get() for p in response.css('p') if 'Óbitos confirmados' in p.get()] or \
-               [p.get() for p in response.css('p') if 'TOTAL DE ÓBITOS' in p.get()]
+    def __get_deaths_paragraph(self, paragraphs):
+        return [p.get() for p in paragraphs if 'Óbitos confirmados' in p.get()] or \
+               [p.get() for p in paragraphs if 'TOTAL DE ÓBITOS' in p.get()]
 
     def __extract_number(self, text_cell):
         raw_number = re.search('[0-9]+\.?[0-9]+', text_cell).group(0)
