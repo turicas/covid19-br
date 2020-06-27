608d477d00011ca85eb8b8a352634d6e0c6d6f2f
Daniel Barreto <danielfsbarreto@gmail.com>
2020-06-27 13:47:37 -0300

    Fix goodtables action (#178)

--- validate.sh
+++ validate.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 set -e
-SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
-source $SCRIPT_PATH/base.sh
+WORKDIR_PATH=$PWD
+DATA_PATH="$WORKDIR_PATH/data/output"
+
+source $WORKDIR_PATH/base.sh
 
 for table in boletim caso caso_full obito_cartorio; do
     echo "Downloading $table"
-    filename="${SCRIPT_PATH}/data/output/${table}.csv.gz"
+    filename="${DATA_PATH}/${table}.csv.gz"
     url="https://data.brasil.io/dataset/covid19/${table}.csv.gz"
     rm -rf "$filename"
     wget -q -c -t 0 -O "$filename" "$url"
 done
 
 echo "Checando integridade dos arquivos..."
-cd "$SCRIPT_PATH/data/output"
-rm -f "SHA512SUMS"
-wget -q -c -t 0 -O "SHA512SUMS" "https://data.brasil.io/dataset/covid19/SHA512SUMS"
+cd $DATA_PATH
+rm -f SHA512SUMS
+wget -q -c -t 0 -O SHA512SUMS "https://data.brasil.io/dataset/covid19/SHA512SUMS"
 sha512sum -c SHA512SUMS
 
-cd $SCRIPT_PATH
-gunzip -fk $SCRIPT_PATH/data/output/*.gz
+echo "Extraindo arquivos..."
+gunzip -fk *.gz
+
+echo "Validando arquivos..."
+cd $WORKDIR_PATH
 goodtables datapackage.json
