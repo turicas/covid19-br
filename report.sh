633710f9820396462b4e5fdebd818611e65c5831
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-07-10 13:24:57 -0300

    Corrige checagem de variáveis de ambiente no relatório

--- report.sh
+++ report.sh
@@ -7,7 +7,7 @@ source $SCRIPT_PATH/base.sh
 log "Creating report..."
 tempfile=$(mktemp)
 time python report.py local | tee $tempfile
-if [ ! -z "$ROCKETCHAT_USER_ID" ] && [ ! -z "$ROCKETCHAT_AUTH_TOKEN=" ]; then
+if [ ! -z "$ROCKETCHAT_USERNAME" ] && [ ! -z "$ROCKETCHAT_PASSWORD" ]; then
 	time python $SCRIPT_PATH/bot/rocketchat.py \
 		--username $ROCKETCHAT_USERNAME \
 		--password $ROCKETCHAT_PASSWORD \
