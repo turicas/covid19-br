bbf399a7f493a0647d6777334a7d9c2885f769aa
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2021-05-17 19:53:34 -0300

    Refaz script de coleta de microdados de vacinação

--- microdados_vacinacao.py
+++ microdados_vacinacao.py
@@ -1,162 +1,82 @@
 import argparse
 import csv
-import logging
+import re
 import sys
-from functools import partial
+from contextlib import closing
+from pathlib import Path
+from urllib.request import urlopen
 
-from async_process_executor import pipeline
-from rows.utils import CsvLazyDictWriter, open_compressed
+import requests
+from lxml.html import document_fromstring
+from rows.utils import CsvLazyDictWriter
 from tqdm import tqdm
 
-from covid19br.elasticsearch import ElasticSearch
-from covid19br.vacinacao import (
-    convert_row_censored,
-    convert_row_uncensored,
-    get_field_converters,
-)
+from covid19br.vacinacao import convert_row_censored, convert_row_uncensored
 
 
-RAW_KEYS = list(get_field_converters().keys())
-
-def get_data_from_elasticsearch(
-    api_url,
-    username,
-    password,
-    index_name,
-    query,
-    sort_by,
-    page_size,
-    ttl,
-):
-    es = ElasticSearch(api_url, username=username, password=password)
-    iterator = es.search(index_name, sort_by=sort_by, page_size=page_size, ttl=ttl, query=query)
-    yield from tqdm(iterator, desc="Consuming ElasticSearch", unit_scale=True)
+REGEXP_DATE = re.compile("([0-9]{4}-[0-9]{2}-[0-9]{2})")
 
 
-def get_data_from_csv(filename, page_size):
-    with open_compressed(filename) as fobj:
-        reader = csv.DictReader(fobj)
-        iterator = rows.utils.ipartition(reader, page_size)
-        for page in tqdm(iterator, unit_scale=True):
-            yield page
+def get_latest_url_and_date():
+    """Scrapes CKAN in order to find the last available CSV for Brazil"""
 
-
-def convert_row_raw(row):
-    new = {key: row.pop(key, None) for key in RAW_KEYS}
-    if row:
-        raise ValueError(f"Unknown keys: {row.keys()}")
-    return new
-
-def convert_rows_page(func, iterator):
-    func = func if func is not None else lambda row: row
-    for page in iterator:
-        yield [func(row) for row in page]
-
-
-def convert_rows(func, iterator):
-    for row in iterator:
-        yield func(row)
-
-
-def write_csv(filename, iterator):
-    writer = CsvLazyDictWriter(filename)
-    for row in iterator:
-        writer.writerow(row)
-
-
-def write_csv_page(filename, iterator):
-    writer = CsvLazyDictWriter(filename)
-    for page in iterator:
-        for row in page:
-            writer.writerow(row)
+    repository_url = "https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/ef3bd0b8-b605-474b-9ae5-c97390c197a8"
+    response = urlopen(repository_url)
+    html = response.read()
+    tree = document_fromstring(html)
+    download_url = tree.xpath("//a[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'dados completos')]/@href")[0]
+    date = REGEXP_DATE.findall(download_url)[0]
+    return download_url, date
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--log-level", default="ERROR")
-    parser.add_argument("--raw", action="store_true")
-    parser.add_argument("--no-censorship", action="store_true")
-    parser.add_argument("--api-url", default="https://imunizacao-es.saude.gov.br/")
-    parser.add_argument("--username", default="imunizacao_public")
-    parser.add_argument("--password", default="qlto5t&7r_@+#Tlstigi")
-    parser.add_argument("--index", default="desc-imunizacao")
-    parser.add_argument("--ttl", default="10m")
-    parser.add_argument("--page-size", default=10_000)
-    parser.add_argument("--input-filename")
-    parser.add_argument("start_datetime")
-    parser.add_argument("end_datetime")
-    parser.add_argument("output_filename")
+    parser.add_argument("--chunk-size", type=int, default=1_024 * 1_024)
+    parser.add_argument("--refresh-count", type=int, default=10_000)
+    parser.add_argument("--input-encoding", type=str, default="utf-8")
     args = parser.parse_args()
 
-    log_level = getattr(logging, args.log_level)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(log_level)
-    handler.setFormatter(
-        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    )
-    pipeline.logger.addHandler(handler)
-    pipeline.logger.setLevel(log_level)
+    # TODO: adicionar opção para selecionar qual dos 3 possíveis CSVs o script
+    # irá gerar.
+    # TODO: configurar saída do logger para arquivo e não stdout/stderr
+    # TODO: adicionar opção para salvar ou não CSV original (compactado)
 
-    convert_row = convert_row_censored
-    if args.raw:
-        convert_row = convert_row_raw
-    elif args.no_censorship:
-        convert_row = convert_row_uncensored
+    url, date = get_latest_url_and_date()
+    output_path = Path(__file__).parent / "data" / "output"
+    filename_raw = output_path / "microdados_vacinacao-raw.csv.gz"
+    filename_censored = output_path / "microdados_vacinacao.csv.gz"
+    filename_uncensored = output_path / "microdados_vacinacao-uncensored.csv.gz"
 
-    if args.input_filename:  # Use local CSV
-        process_pipeline = [
-            (
-                get_data_from_csv,
-                (args.input_filename, args.page_size),
-            ),
-            (
-                partial(convert_rows_page, convert_row),
-                tuple(),
-            ),
-            (
-                write_csv_page,
-                (args.output_filename,),
-            ),
-        ]
+    with closing(requests.get(url, stream=True)) as response:
+        writer_raw = CsvLazyDictWriter(filename_raw)
+        raw_writerow = writer_raw.writerow
+        writer_censored = CsvLazyDictWriter(filename_censored)
+        censored_writerow = writer_censored.writerow
+        writer_uncensored = CsvLazyDictWriter(filename_uncensored)
+        uncensored_writerow = writer_uncensored.writerow
 
-    else:  # Get data from ElasticSearch API
-        timestamp_key = "vacina_dataAplicacao"
-        query = {
-            "range": {
-                timestamp_key: {
-                    "gte": args.start_datetime,
-                    "lt": args.end_datetime,
-                },
-            },
-        }
-        if args.start_datetime == "None":
-            del query["range"][timestamp_key]["gte"]
+        file_size = response.headers.get("content-length")
+        if file_size is not None:
+            file_size = int(file_size)
 
-        process_pipeline = [
-            (
-                get_data_from_elasticsearch,
-                (
-                    args.api_url,
-                    args.username,
-                    args.password,
-                    args.index,
-                    query,
-                    timestamp_key,
-                    args.page_size,
-                    args.ttl,
-                ),
-            ),
-            (
-                partial(convert_rows, convert_row),
-                tuple(),
-            ),
-            (
-                write_csv,
-                (args.output_filename,),
-            ),
-        ]
-
-    pipeline.execute(process_pipeline)
+        refresh_count = args.refresh_count
+        fobj = (
+            line.decode(args.input_encoding)
+            for line in response.iter_lines(chunk_size=args.chunk_size)
+        )
+        reader = csv.DictReader(fobj, delimiter=";")
+        progress = tqdm(reader, total=file_size, unit_scale=True, unit="B")
+        for counter, row in enumerate(reader):
+            raw_writerow(row)
+            censored_writerow(convert_row_censored(row))
+            uncensored_writerow(convert_row_uncensored(row))
+            if counter % refresh_count == 0:
+                progress.n = response.raw.tell()
+                progress.refresh()
+        progress.n = response.raw.tell()
+        progress.close()
+        writer_censored.close()
+        writer_uncensored.close()
 
 
 if __name__ == "__main__":
