7dc6d31e63bb9207c78ca074193e5c094d5b4a36
dehatanes <debs.outsmart@gmail.com>
2022-02-20 00:52:35 -0300

    [Feat][SP] get official total + new sources

--- covid19br/spiders/spider_sp.py
+++ covid19br/spiders/spider_sp.py
@@ -1,7 +1,7 @@
 import csv
 import io
 import logging
-
+import rows
 import scrapy
 
 from covid19br.common.base_spider import BaseCovid19Spider
@@ -9,6 +9,7 @@ from covid19br.common.constants import State, ReportQuality
 from covid19br.common.models.bulletin_models import (
     CountyBulletinModel,
     ImportedUndefinedBulletinModel,
+    StateTotalBulletinModel,
 )
 
 logger = logging.getLogger(__name__)
@@ -28,23 +29,96 @@ class SpiderSP(BaseCovid19Spider):
         ReportQuality.UNDEFINED_OR_IMPORTED_CASES,
     ]
 
-    base_url = "https://www.seade.gov.br"
-    start_urls = [f"{base_url}/coronavirus/"]
+    total_url = "https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/sp.csv"
+    source_1_url = "https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/dados_covid_sp.csv"
+    source2_base_url = "https://www.seade.gov.br"
 
     def pre_init(self):
-        logger.warning(
-            "This spider only gathers data for the current date. "
-            "It does not consider any given 'requested_dates'."
-        )
+        self.requested_dates = list(self.requested_dates)
 
-    def parse(self, response, **kwargs):
+    def start_requests(self):
+        yield scrapy.Request(url=self.total_url, callback=self.parse_csv_total)
+        yield scrapy.Request(url=self.source_1_url, callback=self.parse_csv_from_source1)
+        if self.today in self.requested_dates:
+            yield scrapy.Request(url=f"{self.source2_base_url}/coronavirus/", callback=self.parse_source2)
+
+    def parse_csv_total(self, response):
+        total_reports = rows.import_from_csv(
+            io.BytesIO(response.body),
+            encoding="utf-8-sig",
+            dialect="excel-semicolon",
+            force_types={
+                "datahora": rows.fields.DateField,
+                "casos_acum": rows.fields.IntegerField,
+                "obitos_acum": rows.fields.IntegerField,
+            },
+        )
+        for report in total_reports:
+            if report.datahora in self.requested_dates:
+                bulletin = StateTotalBulletinModel(
+                    date=report.datahora,
+                    state=self.state,
+                    confirmed_cases=report.casos_acum,
+                    deaths=report.obitos_acum,
+                    source_url=response.request.url,
+                )
+                self.add_new_bulletin_to_report(bulletin, report.datahora)
+
+    def parse_csv_from_source1(self, response):
+        county_reports = rows.import_from_csv(
+            io.BytesIO(response.body),
+            encoding="utf-8-sig",
+            dialect="excel-semicolon",
+            force_types={
+                "nome_munic": rows.fields.TextField,
+                "datahora": rows.fields.DateField,
+                "casos": rows.fields.IntegerField,
+                "obitos": rows.fields.IntegerField,
+                "codigo_ibge": rows.fields.TextField,
+                "casos_novos": rows.fields.IntegerField,
+                "casos_pc": rows.fields.TextField,
+                "casos_mm7d": rows.fields.TextField,
+                "obitos_novos": rows.fields.TextField,
+                "obitos_pc": rows.fields.TextField,
+                "obitos_mm7d": rows.fields.TextField,
+                "letalidade": rows.fields.TextField,
+                "nome_ra": rows.fields.TextField,
+                "cod_ra": rows.fields.TextField,
+                "nome_drs": rows.fields.TextField,
+                "cod_drs": rows.fields.IntegerField,
+                "pop": rows.fields.TextField,
+                "semana_epidem": rows.fields.TextField,
+            },
+        )
+        for report in county_reports:
+            if report.datahora in self.requested_dates:
+                if report.nome_munic == 'Ignorado':
+                    bulletin = ImportedUndefinedBulletinModel(
+                        date=report.datahora,
+                        state=self.state,
+                        confirmed_cases=report.casos,
+                        deaths=report.obitos,
+                        source_url=response.request.url,
+                    )
+                else:
+                    bulletin = CountyBulletinModel(
+                        date=report.datahora,
+                        state=self.state,
+                        city=report.nome_munic,
+                        confirmed_cases=report.casos,
+                        deaths=report.obitos,
+                        source_url=response.request.url,
+                    )
+                self.add_new_bulletin_to_report(bulletin, report.datahora)
+
+    def parse_source2(self, response):
         csv_path = response.xpath(
             "//a[contains(@href, '.csv') and contains(span/text(), 'Municípios')]/@href"
         ).extract_first()
-        csv_url = self.base_url + csv_path
-        yield scrapy.Request(csv_url, callback=self.parse_csv)
+        csv_url = self.source2_base_url + csv_path
+        yield scrapy.Request(csv_url, callback=self.parse_csv_from_source2)
 
-    def parse_csv(self, response, **kwargs):
+    def parse_csv_from_source2(self, response):
         reader = csv.DictReader(
             io.StringIO(response.body.decode("iso-8859-1")), delimiter=";"
         )
