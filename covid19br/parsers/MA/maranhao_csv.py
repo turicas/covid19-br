eeb7da24e910b2053bbd0924cd89d30fae592cbb
dehatanes <debs.outsmart@gmail.com>
2022-03-14 22:16:13 -0300

    [Spider MA] Fix csv extractor for different encodings

--- covid19br/parsers/MA/maranhao_csv.py
+++ covid19br/parsers/MA/maranhao_csv.py
@@ -7,24 +7,36 @@ DEATH_CASES_CSV_COLUMN = 2
 
 class MaranhaoCSVBulletinExtractor:
     def __init__(self, filename):
-        self.filename = filename
+        with open(filename, encoding="latin-1") as fobj:
+            reader = csv.reader(fobj, delimiter=";", lineterminator="\n")
+            self.file_content = list(reader)
+
+        # We actually just need the header, but we are not sure about
+        # it's position so we take the first 10 lines
+        first_items = [l[0].strip() for l in self.file_content[:10]]
+        # If the encoding is wrong, we try another one
+        if "MUNICÍPIOS" not in first_items:
+            with open(filename, encoding="mac_iceland") as fobj:
+                reader = csv.reader(fobj, delimiter=";", lineterminator="\n")
+                self.file_content = list(reader)
 
     @property
     def data(self):
-        with open(self.filename, encoding="mac_iceland") as fobj:
-            reader = csv.reader(fobj, delimiter=";", lineterminator="\n")
-            header = (
-                None
-            )  # There are blank lines in the begining of the file. Wait until find the header
-            for line in reader:
-                # Consider only non-empty lines and first 3 columns
-                line = [cell.strip() for cell in line[:3]]
-                if not any(line):
-                    continue
-                elif not header:
-                    if line[CITY_NAME_CSV_COLUMN] == "MUNICÍPIOS":
-                        header = line
-                    continue
+        # There are blank lines in the beginning of the file. Wait until find the header
+        header = None
+        for line in self.file_content:
+            # Consider only non-empty lines and first 3 columns
+            line = [cell.strip() for cell in line[:3]]
+            if not any(line):
+                continue
+            elif not header:
+                if line[CITY_NAME_CSV_COLUMN] == "MUNICÍPIOS":
+                    header = line
+                continue
+
+            if line[
+                CITY_NAME_CSV_COLUMN
+            ]:  # There are some blank lines we want to ignore
                 yield {
                     "municipio": line[CITY_NAME_CSV_COLUMN],
                     "confirmados": line[CONFIRMED_CASES_CSV_COLUMN],
